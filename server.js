0fae32ef43ed2a8d2aea51188b1b37fa886d87c6
CodingNav <navleenkaurr@gmail.com>
2020-12-06 12:37:49 -0500

    Set up static files

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+var path = require("path");
 var express = require('express')
 var app = express()
 var api = require('./api-routes.js')
 
+var PORT = process.env.PORT || 3000;
+
 app.use('/api', api)
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
 })
  
-app.listen(3000)
+app.listen(PORT, function() {
+    console.log("App Listening on: http://localhost:"+PORT)
+})
