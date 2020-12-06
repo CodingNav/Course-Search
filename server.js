020effab095bdf62f8112242471b930da05b2aba
CodingNav <navleenkaurr@gmail.com>
2020-12-06 16:09:49 -0500

    Added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requiring packages
 var path = require("path");
 var express = require('express')
+var cors = require('cors')
 var app = express()
 
 // Requiring api-routes file
@@ -8,11 +9,16 @@ var api = require('./api-routes.js')
 
 var PORT = process.env.PORT || 3000;
 
+
 // Connects express to api routes file
 app.use('/api', api)
 
+
 // Allows static content within public folder to load
 app.use(express.static('public'))
+
+// Allows for other sites to use my project's resources 
+app.use(cors())
 
 // GET route for the index.html
 app.get('/', function (req, res) {
